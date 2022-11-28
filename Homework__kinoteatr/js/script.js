7905f6523bebfc50c3463d121c3030008878d572
AbbosJo'rayev[FE16] <118529401+abbos-jurayev@users.noreply.github.com>
2022-11-28 14:37:06 +0500

    Add files via upload

---
+++ Homework__kinoteatr/js/script.js
@@ -0,0 +1,62 @@
+// let age = +prompt("yoshingiz nechchida");
+// let money = +prompt("qancha pulingiz bor");
+// let theater = 0;
+// if(age >= 18 && money >= 30){
+//     console.log("Kirishingiz mumkin");
+//     let kino = +prompt('filmni tanlang: 1-qasoskorlar,2-shazam,3-superman,4-thor')
+// 	switch (kino) {
+//         case 1:
+//             console.log("siz qasoskorlar filimini tandadingiz")
+//             alert("yoqimli hordiq tilayman")
+//             break;
+//         case 2:
+//             console.log("siz shazam filimini tandadingiz")
+//             alert("yoqimli hordiq tilayman");
+//             break;
+//         case 3:
+//             console.log("siz superman filimini tandadingiz")
+//             alert("yoqimli hordiq tilayman");
+//             break;
+//         case 4:
+//             console.log("siz thor filimini tandadingiz")
+//             alert("yoqimli hordiq tilayman");
+//     }
+// }
+// else{
+//     console.log("Kirishingiz mumkin emas")
+// }
+
+
+// let day = prompt('1dan 7gacha son kiriting');
+// switch (day){
+//     case "1":
+//     alert('dushanba');
+//     break;
+//     case "2":
+//     alert('seshanba');
+//     break;
+//     case "3":
+//     alert('chorshanba');
+//     break;
+//     case "4":
+//     alert('payshanba');
+//     break;
+//     case "5":
+//     alert('juma');
+//     break;
+//     case "6":
+//     alert('shanba');
+//     break;
+//     case "7":
+//     alert('yakshanba');
+//     break;
+//     default:
+//             alert("bunday hafta kuni yuq")
+//         }
+
+// let logo = +prompt('istagan raqamizni kiriting');
+// if (logo % 2 == 0) {
+//     alert('bu juft son');
+// } else {
+//     alert('bu toq son');
+// }
